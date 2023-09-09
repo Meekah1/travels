c97fa8747c2832c16608726d3f4db88d60128a71
Abdulakeem Bolarinwa <bolarinwa_akeem@yahoo.com>
2023-09-09 15:04:31 +0100

    lil addition

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import AccordionItems from "./AccordionItem";
 
 
 
 export default function Accordion({data, onCloseIcon}) {
+ const [curOpen, setCurOpen] = useState(null)
+
+
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItems onCloseIcon={onCloseIcon} title={el.title} text={el.text} num={i}/>
+        <AccordionItems curOpen={curOpen} onOpen={setCurOpen} onCloseIcon={onCloseIcon} title={el.title}  num={i}>
+          {el.text}
+        </AccordionItems>
+        
       ))}
+      <AccordionItems curOpen={curOpen} onOpen={setCurOpen} onCloseIcon={onCloseIcon} title="Test 2"  num={22}>
+          <p>Things to achieve</p>
+          <ul>
+            <li>React is beautiful</li>
+            <li>I wil be a force to reckon with and also a multi millionaire in Dollars in Software Development world by Almighty Allah's Grace</li>
+            <li>I will always propagate islam whenever i am opportuned to address the world</li>
+          </ul>
+        </AccordionItems>
     </div>
   );
 }
--- src/AccordionItem.js
+++ src/AccordionItem.js
@@ -1,11 +1,13 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 
-export default function AccordionItems({ num, text, title}) {
-  const [isOpen, setIsOpen] = useState(false)
+export default function AccordionItems({ num, text, title, curOpen, onOpen, children }) {
+  // const [isOpen, setIsOpen] = useState(false)
 
-  function handleOpen () {
- setIsOpen(!isOpen)
+  const isOpen = num === curOpen
+
+  function handleOpen() {
+    onOpen(isOpen ? null : num)
   }
 
   return (
@@ -13,8 +15,8 @@ export default function AccordionItems({ num, text, title}) {
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon" >{isOpen ? "-" : "+"}</p>
-      {isOpen ? 
-        <p className="content-box">{text}</p> : ""}
+      {isOpen ?
+        <p className="content-box">{children}</p> : ""}
     </div>
   );
 }
